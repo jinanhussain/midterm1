af5906a5c7da908958be9e4ab461d967eeed5a24
Jinan Hussain <jinanhussain@jinans-mbp.mynetworksettings.com>
2024-10-29 22:04:27 -0400

    add files

--- main.py
+++ main.py
@@ -12,11 +12,17 @@ class MainApp:
     def start(self):
         while True:
             value = input("Enter an input: ")
+
+
             userInput = value.split() # add 1 3 ['add', 1, 3]
             cmd = userInput[0]
+            
             num1 = userInput[1]
             num2 = userInput[2]
-            commandList = {"add": AddCommand, "subtract": SubtractCommand, "multiply": MultiplyCommand, "divide": DivideCommand }
+
+
+            # historyCommandList = {"load": LoadCommand, "save" }
+            commandList = {"add": AddCommand, "subtract": SubtractCommand, "multiply": MultiplyCommand, "divide": DivideCommand}
             try:
                 commandClass = commandList[cmd]
                 commandClass.execute(self, params=(num1, num2))
@@ -24,6 +30,9 @@ class MainApp:
                 print("failed")
 
             ## try to append cmd, num1, num2 to history file
+
+            ## create plugins for Load, save, clear, and delete 
+            appendToHistory(cmd, num1, num2)
 
 
             # AddCommand.execute(self, params=(num1, num2))
