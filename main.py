c4745afb5a6b62871f1e873dba679581a3886ec7
Jinan Hussain <jinanhussain@jinans-mbp.mynetworksettings.com>
2024-10-29 20:13:21 -0400

    add files

---
+++ main.py
@@ -0,0 +1,13 @@
+class MainApp:
+    def __init__(self):
+        ## make environment variables and stuff
+        return
+    def start(self):
+        ## write code here
+        print('hello werld')
+        return
+
+    
+if __name__ == "__main__":
+    app = MainApp()
+    app.start()
