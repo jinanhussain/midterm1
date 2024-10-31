3e4ce205199c75bff24526ea076a633a6199dbe0
Jinan Hussain <jinanhussain@jinans-mbp.mynetworksettings.com>
2024-10-31 19:17:21 -0400

    add files

--- plugins/read_history/__init__.py
+++ plugins/read_history/__init__.py
@@ -1,7 +1,9 @@
 from commands import Command
 import pandas as pd
 
-def readHistory(self): 
+class ReadHistoryCommand(Command):
+    def execute(self, params):
+        print("clearing")
         file_path = 'data/history.csv'
         df = pd.read_csv(file_path)
         print(df) 
